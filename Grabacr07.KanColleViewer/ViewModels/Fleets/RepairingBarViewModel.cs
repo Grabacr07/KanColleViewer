b096b3e72c2ad3ec97b40e80a42acf20d07754d1
fantasticswallow <f_swallow@live.com>
2013-12-14 17:06:19 +0900

    出撃欄の入渠完了時間、が UTC で表示されるのを現地時刻にするように

--- Grabacr07.KanColleViewer/ViewModels/Fleets/RepairingBarViewModel.cs
+++ Grabacr07.KanColleViewer/ViewModels/Fleets/RepairingBarViewModel.cs
@@ -32,9 +32,9 @@ namespace Grabacr07.KanColleViewer.ViewModels.Fleets
 			{
 				return "艦隊に入渠中の艦娘がいます。";
 			}
-			var remaining = dock.CompleteTime.Value - DateTimeOffset.Now - TimeSpan.FromMinutes(1.0);
-			return string.Format(@"艦隊に入渠中の艦娘がいます。 完了時刻: {0:MM/dd HH\:mm} 完了まで: {1}:{2:mm\:ss}",
-				dock.CompleteTime.Value, (int) remaining.TotalHours, remaining);
+            var remaining = dock.CompleteTime.Value.LocalDateTime - DateTimeOffset.Now - TimeSpan.FromMinutes(1.0);
+            return string.Format(@"艦隊に入渠中の艦娘がいます。 完了時刻: {0:MM/dd HH\:mm} 完了まで: {1}:{2:mm\:ss}",
+                dock.CompleteTime.Value.LocalDateTime, (int)remaining.TotalHours, remaining);
 		}
 	}
 }
