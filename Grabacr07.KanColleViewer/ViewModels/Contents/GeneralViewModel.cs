d76a8ed11d62860d36d4c7fdd7d9bb548a84434a
Grabacr07 <grabacr07+github@gmail.com>
2014-01-03 05:47:07 +0900

    General View 関連クラスの作成、UI 試作

---
+++ Grabacr07.KanColleViewer/ViewModels/Contents/GeneralViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Livet;
+
+namespace Grabacr07.KanColleViewer.ViewModels.Contents
+{
+	public class GeneralViewModel : TabItemViewModel
+	{
+		public MainContentViewModel Content { get; private set; }
+
+		public GeneralViewModel(MainContentViewModel owner)
+		{
+			this.Content = owner;
+		}
+	}
+}
