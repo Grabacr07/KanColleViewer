285cbf1fff8cac4b55829d7b326824a4fee9bbcb
SquallATF <squallatf@gmail.com>
2014-01-10 12:32:53 +0800

    right way to deserialize kcsapi_ship3 api_slot_data

--- Grabacr07.KanColleWrapper/Models/Raw/kcsapi_ship3.cs
+++ Grabacr07.KanColleWrapper/Models/Raw/kcsapi_ship3.cs
@@ -11,9 +11,7 @@ namespace Grabacr07.KanColleWrapper.Models.Raw
 	{
 		public kcsapi_ship2[] api_ship_data { get; set; }
 		public kcsapi_deck[] api_deck_data { get; set; }
-
-		// 今のところ必要ないので
-		//public kcsapi_slot_data api_slot_data { get; set; }
+		public Dictionary<string, object> api_slot_data { get; set; }
 	}
 	// ReSharper restore InconsistentNaming
 }
