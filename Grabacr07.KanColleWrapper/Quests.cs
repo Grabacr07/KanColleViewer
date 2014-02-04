581e95c4ae94ac7e5bc84f5a749ecb2d830507a3
FreyYa <hong09072@gmail.com>
2014-02-05 08:02:26 +0900

    任務の読み込みに失敗する問題の修正

--- Grabacr07.KanColleWrapper/Quests.cs
+++ Grabacr07.KanColleWrapper/Quests.cs
@@ -172,17 +172,12 @@ namespace Grabacr07.KanColleWrapper
 			{
 				while (this.questPages.Count < questlist.api_page_count) this.questPages.Add(null);
 			}
-            //decimal calc_quest_page;
-            if (questlist.api_disp_page - 1 < 0)
-            {
-                questlist.api_disp_page = questlist.api_page_count;
-                this.questPages[questlist.api_disp_page] = new ConcurrentDictionary<int, Quest>();
-            }
 
-            else
-            {
-                this.questPages[questlist.api_disp_page - 1] = new ConcurrentDictionary<int, Quest>();
-            }
+			var page = questlist.api_disp_page - 1;
+			if (page >= this.questPages.Count) page = this.questPages.Count - 1;
+
+			this.questPages[page] = new ConcurrentDictionary<int, Quest>();
+
 
 			if (questlist.api_list == null)
 			{
@@ -194,7 +189,7 @@ namespace Grabacr07.KanColleWrapper
 				this.IsEmpty = false;
 
 				questlist.api_list.Select(x => new Quest(x))
-					.ForEach(x => this.questPages[questlist.api_disp_page - 1].AddOrUpdate(x.Id, x, (_, __) => x));
+					.ForEach(x => this.questPages[page].AddOrUpdate(x.Id, x, (_, __) => x));
 
 				this.All = this.questPages.Where(x => x != null)
 					.SelectMany(x => x.Select(kvp => kvp.Value))
