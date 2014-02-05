3a46a250c93d9142c175ca22d85b80f5b4cad3b8
FreyYa <hong09072@gmail.com>
2014-02-05 10:42:24 +0900

    Win7 Notification(Sound Add) +notify.wavファイルを追加して、通知音として使用することができます。win7で作動かどうかはまだtest中です。

--- Grabacr07.KanColleViewer/Models/Internal/Windows7Notifier.cs
+++ Grabacr07.KanColleViewer/Models/Internal/Windows7Notifier.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
+using System.Media;
+using System.IO;
+using Grabacr07.KanColleViewer.Models;
 using Application = System.Windows.Application;
 
-namespace Grabacr07.KanColleViewer.Models.Internal
-{
+
+namespace Grabacr07.KanColleViewer.Models.Internal{
 	/// <summary>
 	/// 通知領域アイコンを利用した通知機能を提供します。
 	/// </summary>
@@ -14,13 +17,14 @@ namespace Grabacr07.KanColleViewer.Models.Internal
 	{
 		private NotifyIcon notifyIcon;
 		private EventHandler activatedAction;
-
+        
 		public void Initialize()
 		{
 			const string iconUri = "pack://application:,,,/KanColleViewer;Component/Assets/app.ico";
-
-			Uri uri;
-			if (!Uri.TryCreate(iconUri, UriKind.Absolute, out uri)) return;
+            
+            Uri uri;
+			
+            if (!Uri.TryCreate(iconUri, UriKind.Absolute, out uri)) return;
 
 			var streamResourceInfo = Application.GetResourceStream(uri);
 			if (streamResourceInfo == null) return;
@@ -42,13 +46,31 @@ namespace Grabacr07.KanColleViewer.Models.Internal
 
 			if (activated != null)
 			{
-				this.notifyIcon.BalloonTipClicked -= this.activatedAction;
+                
+                
+				
+                this.notifyIcon.BalloonTipClicked -= this.activatedAction;
 
 				this.activatedAction = (sender, args) => activated();
 				this.notifyIcon.BalloonTipClicked += this.activatedAction;
 			}
+            //소리재생 해봅시다 한번
+            var location = System.Reflection.Assembly.GetEntryAssembly().Location;
+            string Main_folder = Path.GetDirectoryName(location);
+            SoundPlayer notify_sound = new SoundPlayer(Main_folder + "\\notify.wav");
+            notify_sound.Load();
+            var checkV=Volume.GetInstance();
 
-			notifyIcon.ShowBalloonTip(1000, header, body, ToolTipIcon.None);
+            if (checkV.IsMute==false && System.IO.File.Exists(Main_folder + "\\notify.wav") == true)
+            {
+                notify_sound.Play();                
+			}
+            
+            else
+            {
+                System.Media.SystemSounds.Beep.Play();                
+            }
+            notifyIcon.ShowBalloonTip(1000, header, body, ToolTipIcon.None);
 		}
 
 		public void Dispose()
