83497bf83bc210c68ce33f3382d9ea42236e4c21
Grabacr07 <grabacr07+github@gmail.com>
2014-02-09 07:18:48 +0900

    装備一覧機能、とりあえずこんなもん版

--- Grabacr07.KanColleViewer/ViewModels/Catalogs/SlotItemCatalogViewModel.cs
+++ Grabacr07.KanColleViewer/ViewModels/Catalogs/SlotItemCatalogViewModel.cs
@@ -1,16 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
-using Grabacr07.KanColleViewer.ViewModels.Contents;
 using Grabacr07.KanColleWrapper;
 using Grabacr07.KanColleWrapper.Models;
 using Livet;
-using Livet.EventListeners;
 
 namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
 {
@@ -35,56 +29,67 @@ namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
 
 		#endregion
 
+		#region IsReloading 変更通知プロパティ
+
+		private bool _IsReloading;
+
+		public bool IsReloading
+		{
+			get { return this._IsReloading; }
+			set
+			{
+				if (this._IsReloading != value)
+				{
+					this._IsReloading = value;
+					this.RaisePropertyChanged();
+				}
+			}
+		}
+
+		#endregion
+
 
 		public SlotItemCatalogViewModel()
 		{
 			this.Title = "所有装備一覧";
-			this.UpdateCore();
+			this.Update();
 		}
 
 
-		private void UpdateCore()
+		public async void Update()
+		{
+			this.IsReloading = true;
+			this.SlotItems = await UpdateCore();
+			this.IsReloading = false;
+		}
+
+		private static Task<List<SlotItemViewModel>> UpdateCore()
 		{
 			// これはひどい
 			// あとでちゃんと書き直す
 
 			var ships = KanColleClient.Current.Homeport.Ships;
 			var items = KanColleClient.Current.Homeport.SlotItems;
-			var dic = KanColleClient.Current.Master.SlotItems.ToDictionary(kvp => kvp.Key, kvp => new SlotItemViewModel(kvp.Value));
+			var master = KanColleClient.Current.Master.SlotItems;
 
-			items.ForEach(x => dic[x.Value.Info.Id].Count++);
-
-			foreach (var ship in ships.Values)
+			return Task.Factory.StartNew(() =>
 			{
-				foreach (var target in ship.SlotItems.Where(x => x != null).Select(item => dic[item.Info.Id]))
+				var dic = items.GroupBy(kvp => kvp.Value.Info.Id, kvp => kvp.Value)
+					.ToDictionary(g => g.Key, g => new SlotItemViewModel { SlotItem = master[g.Key], Count = g.Count() });
+
+				foreach (var ship in ships.Values)
 				{
-					target.Ships.Add(ship);
+					foreach (var target in ship.SlotItems.Where(x => x != null).Select(item => dic[item.Info.Id]))
+					{
+						target.AddShip(ship);
+					}
 				}
-			}
 
-			this.SlotItems = dic.Values.Where(x => x.Count > 0).ToList();
-		}
-	}
-
-
-	public class SlotItemViewModel : ViewModel
-	{
-		public SlotItemInfo SlotItem { get; set; }
-		public int Count { get; set; }
-		public List<Ship> Ships { get; set; }
-
-		public SlotItemViewModel(SlotItemInfo info)
-		{
-			this.SlotItem = info;
-			this.Ships = new List<Ship>();
-		}
-
-		public override string ToString()
-		{
-			return string.Format(
-				"{0} - Count: {1} / Ships: {2}", 
-				this.SlotItem.Name, this.Count, 
-				string.Join(",", this.Ships.Select(x => x.Info.Name + " (Lv." + x.Level + ")")));
+				return dic.Values
+					.OrderBy(x => x.SlotItem.CategoryId)
+					.ThenBy(x => x.SlotItem.Id)
+					.ToList();
+			});
 		}
 	}
 }
