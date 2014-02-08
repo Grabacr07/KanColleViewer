7a7e2d1cdd7e6593e6bbf2f2eb7548b832e98b09
P3PPP <ticktackmob@gmail.com>
2014-02-09 00:13:38 +0900

    コーディングスタイル遵守のため、ソフトタブをハードタブに修正。

--- Grabacr07.KanColleViewer/Views/Behaviors/FlickTrigger.cs
+++ Grabacr07.KanColleViewer/Views/Behaviors/FlickTrigger.cs
@@ -10,71 +10,71 @@ using System.Windows.Media;
 
 namespace Grabacr07.KanColleViewer.Views.Behaviors
 {
-    public enum FlickDirection
-    {
-        Up,
-        Down,
-        Right,
-        Left
-    }
+	public enum FlickDirection
+	{
+		Up,
+		Down,
+		Right,
+		Left
+	}
 
-    public class FlickTrigger : TriggerBase<UIElement>
-    {
-        private StylusPointCollection downPoints;
+	public class FlickTrigger : TriggerBase<UIElement>
+	{
+		private StylusPointCollection downPoints;
 
-        protected override void OnAttached()
-        {
-            base.OnAttached();
-            this.AssociatedObject.StylusDown += OnStylusDown;
-            this.AssociatedObject.StylusSystemGesture += OnStylusSystemGesture;
-        }
+		protected override void OnAttached()
+		{
+			base.OnAttached();
+			this.AssociatedObject.StylusDown += OnStylusDown;
+			this.AssociatedObject.StylusSystemGesture += OnStylusSystemGesture;
+		}
 
-        protected override void OnDetaching()
-        {
-            this.AssociatedObject.StylusDown -= OnStylusDown;
-            this.AssociatedObject.StylusSystemGesture -= OnStylusSystemGesture;
-            base.OnDetaching();
-        }
+		protected override void OnDetaching()
+		{
+			this.AssociatedObject.StylusDown -= OnStylusDown;
+			this.AssociatedObject.StylusSystemGesture -= OnStylusSystemGesture;
+			base.OnDetaching();
+		}
 
-        private void OnStylusSystemGesture(object sender, StylusSystemGestureEventArgs e)
-        {
-            if (e.SystemGesture != SystemGesture.Drag)
-                return;
+		private void OnStylusSystemGesture(object sender, StylusSystemGestureEventArgs e)
+		{
+			if (e.SystemGesture != SystemGesture.Drag)
+				return;
 
-            if (this.downPoints == null)
-                return;
+			if (this.downPoints == null)
+				return;
 
-            var hitTestResult = VisualTreeHelper.HitTest(this.AssociatedObject, downPoints[0].ToPoint());
-            if (hitTestResult == null)
-                return;
+			var hitTestResult = VisualTreeHelper.HitTest(this.AssociatedObject, downPoints[0].ToPoint());
+			if (hitTestResult == null)
+				return;
 
-            var newPoints = e.GetStylusPoints(this.AssociatedObject);
-            if (newPoints.Count <= 0 || this.downPoints.Count <= 0)
-                return;
+			var newPoints = e.GetStylusPoints(this.AssociatedObject);
+			if (newPoints.Count <= 0 || this.downPoints.Count <= 0)
+				return;
 
-            if (this.AssociatedObject is Button)
-                this.AssociatedObject.ReleaseStylusCapture();
+			if (this.AssociatedObject is Button)
+				this.AssociatedObject.ReleaseStylusCapture();
 
-            var distanceX = newPoints[0].X - this.downPoints[0].X;
-            var distanceY = newPoints[0].Y - this.downPoints[0].Y;
+			var distanceX = newPoints[0].X - this.downPoints[0].X;
+			var distanceY = newPoints[0].Y - this.downPoints[0].Y;
 
-            if (Math.Abs(distanceX) > Math.Abs(distanceY))
-            {
-                this.InvokeActions(distanceX < 0
-                    ? FlickDirection.Left
-                    : FlickDirection.Right);
-            }
-            else
-            {
-                this.InvokeActions(distanceY < 0
-                    ? FlickDirection.Down
-                    : FlickDirection.Up);
-            }
-        }
+			if (Math.Abs(distanceX) > Math.Abs(distanceY))
+			{
+				this.InvokeActions(distanceX < 0
+					? FlickDirection.Left
+					: FlickDirection.Right);
+			}
+			else
+			{
+				this.InvokeActions(distanceY < 0
+					? FlickDirection.Down
+					: FlickDirection.Up);
+			}
+		}
 
-        private void OnStylusDown(object sender, StylusDownEventArgs e)
-        {
-            this.downPoints = e.GetStylusPoints(AssociatedObject);
-        }
-    }
+		private void OnStylusDown(object sender, StylusDownEventArgs e)
+		{
+			this.downPoints = e.GetStylusPoints(AssociatedObject);
+		}
+	}
 }
