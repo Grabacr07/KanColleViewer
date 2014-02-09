f23d79aa5d88f40e89d383031c24c5061fc73671
FreyYa <hong09072@gmail.com>
2014-02-09 20:51:24 +0900

    translated and ver 2.1.0

--- Grabacr07.KanColleViewer/Properties/AssemblyInfo.cs
+++ Grabacr07.KanColleViewer/Properties/AssemblyInfo.cs
@@ -51,4 +51,4 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
