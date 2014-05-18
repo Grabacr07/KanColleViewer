d74cd210451cb27c6025a5be6a888463ef7fd714
Grabacr07 <grabacr07+github@gmail.com>
2014-05-18 15:45:55 +0900

    ・ツールタブを追加 ・補給と装備破棄の回数をカウントするツールを追加

--- Grabacr07.KanColleViewer/ViewModels/MainContentViewModel.cs
+++ Grabacr07.KanColleViewer/ViewModels/MainContentViewModel.cs
@@ -71,6 +71,7 @@ namespace Grabacr07.KanColleViewer.ViewModels
 				this.Dockyard,
 				this.Quests,
 				this.Expeditions,
+				new ToolsViewModel(),
 			};
 			this.SystemTabItems = new List<TabItemViewModel>
 			{
---
+++ Grabacr07.KanColleViewer/ViewModels/ToolsViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Grabacr07.KanColleViewer.Properties;
+using Grabacr07.KanColleWrapper;
+using Livet;
+
+namespace Grabacr07.KanColleViewer.ViewModels
+{
+	public class ToolsViewModel : TabItemViewModel
+	{
+		public override string Name
+		{
+			get { return "ツール"; }
+			protected set { throw new NotImplementedException(); }
+		}
+
+		#region Counters 変更通知プロパティ
+
+		private ObservableCollection<CounterBase> _Counters;
+
+		public ObservableCollection<CounterBase> Counters
+		{
+			get { return this._Counters; }
+			set
+			{
+				if (this._Counters != value)
+				{
+					this._Counters = value;
+					this.RaisePropertyChanged();
+				}
+			}
+		}
+
+		#endregion
+
+
+		public ToolsViewModel()
+		{
+			this.Counters = new ObservableCollection<CounterBase>
+			{
+				new SupplyCounter(KanColleClient.Current.Proxy),
+				new ItemDestroyCounter(KanColleClient.Current.Proxy),
+			};
+		}
+	}
+}
