dc698550db9d0e155d4a1ae4b770aa4e3387c444
Grabacr07 <grabacr07+github@gmail.com>
2014-07-16 13:07:26 +0900

    csproj の移動と改行コードの標準化

--- Plugins/Counter/Properties/AssemblyInfo.cs
+++ Plugins/Counter/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("Grabacr07.Desktop.Metro")]
-[assembly: AssemblyDescription("Grabacr07.Desktop.Metro")]
+[assembly: AssemblyTitle("Counter")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("grabacr.net")]
-[assembly: AssemblyProduct("Grabacr07.Desktop.Metro")]
-[assembly: AssemblyCopyright("Copyright c 2013 Grabacr07")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Counter")]
+[assembly: AssemblyCopyright("Copyright c  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,13 +31,13 @@ using System.Windows;
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
 
 
-[assembly: ThemeInfo(
-	ResourceDictionaryLocation.None, //テーマ固有のリソース ディクショナリが置かれている場所
-	//(リソースがページ、
-	// またはアプリケーション リソース ディクショナリに見つからない場合に使用されます)
-	ResourceDictionaryLocation.SourceAssembly //汎用リソース ディクショナリが置かれている場所
-	//(リソースがページ、
-	// アプリケーション、またはいずれのテーマ固有のリソース ディクショナリにも見つからない場合に使用されます)
+[assembly:ThemeInfo(
+    ResourceDictionaryLocation.None, //テーマ固有のリソース ディクショナリが置かれている場所
+                             //(リソースがページ、
+                             //またはアプリケーション リソース ディクショナリに見つからない場合に使用されます)
+    ResourceDictionaryLocation.SourceAssembly //汎用リソース ディクショナリが置かれている場所
+                                      //(リソースがページ、
+                                      //アプリケーション、またはいずれのテーマ固有のリソース ディクショナリにも見つからない場合に使用されます)
 )]
 
 
