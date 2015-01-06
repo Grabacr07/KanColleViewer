6e78dbfbf8ed52175a00e0582611357ac90f24da
FreyYa <hong09072@gmail.com>
2015-01-06 15:46:23 +0900

    Remove Width Binding

--- Plugins/BattlePreview/ViewModels/BattlePreviewsViewModel.cs
+++ Plugins/BattlePreview/ViewModels/BattlePreviewsViewModel.cs
@@ -28,44 +28,6 @@ namespace Grabacr07.KanColleViewer.Plugins.ViewModels
 
 		#endregion
 
-		#region IsCombined 変更通知プロパティ
-
-		private double _IsCombined;
-
-		public double IsCombined
-		{
-			get { return this._IsCombined; }
-			set
-			{
-				if (this._IsCombined != value)
-				{
-					this._IsCombined = value;
-					this.RaisePropertyChanged();
-				}
-			}
-		}
-
-		#endregion
-
-		#region IsRefreshed 変更通知プロパティ
-
-		private double _IsRefreshed;
-
-		public double IsRefreshed
-		{
-			get { return this._IsRefreshed; }
-			set
-			{
-				if (this._IsRefreshed != value)
-				{
-					this._IsRefreshed = value;
-					this.RaisePropertyChanged();
-				}
-			}
-		}
-
-		#endregion
-
 		#region Rank 変更通知プロパティ
 
 		private string _Rank;
@@ -100,18 +62,6 @@ namespace Grabacr07.KanColleViewer.Plugins.ViewModels
 					{
 						this.ResultReport = new List<PreviewBattleResults>(KanColleClient.Current.PreviewBattle.TotalResult());
 						this.Rank = this.ResultReport[6].Rank;
-						
-						//연합함대가 편성되었을때는 연합함대 항목을 확장시켜준다. 임시코드
-						if (KanColleClient.Current.Homeport.Organization.Combined)
-						{
-							this.IsCombined = (double)1;
-							this.IsCombined = double.NaN;
-						}
-						else this.IsCombined = 0.0;
-
-						//항목이 사라지는것에 대한 임시조치
-						this.IsRefreshed = (double)1;
-						this.IsRefreshed = double.NaN;
 					};
 				}
 			}
