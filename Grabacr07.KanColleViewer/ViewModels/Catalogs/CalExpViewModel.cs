11e0a95e7a8d97a38a7064d9d698f0598f1de623
FreyYa <hong09072@gmail.com>
2015-02-05 19:49:35 +0900

    Add Calc Sea Area

--- Grabacr07.KanColleViewer/ViewModels/Catalogs/CalExpViewModel.cs
+++ Grabacr07.KanColleViewer/ViewModels/Catalogs/CalExpViewModel.cs
@@ -32,13 +32,14 @@ namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
 		{
 			{"1-1", 30}, {"1-2", 50}, {"1-3", 80}, {"1-4", 100}, {"1-5", 150},
 			{"2-1", 120}, {"2-2", 150}, {"2-3", 200},{"2-4", 300},{"2-5", 250},
-			{"3-1", 310}, {"3-2", 320}, {"3-3", 330}, {"3-4", 350},
+			{"3-1", 310}, {"3-2", 320}, {"3-3", 330}, {"3-4", 350},{"3-5",400},
 			{"4-1", 310}, {"4-2", 320}, {"4-3", 330}, {"4-4", 340},
-			{"5-1", 360}, {"5-2", 380}, {"5-3", 400}, {"5-4", 420}, {"5-5", 450}
+			{"5-1", 360}, {"5-2", 380}, {"5-3", 400}, {"5-4", 420}, {"5-5", 450},
+			{"6-1", 380}, {"6-2", 420}
 		};
 
 		public IEnumerable<string> ResultList { get; private set; }
-		public string[] Results =  { "S", "A", "B", "C", "D", "E" };
+		public string[] Results = { "S", "A", "B", "C", "D", "E" };
 
 		private readonly Subject<Unit> updateSource = new Subject<Unit>();
 		private readonly Homeport homeport = KanColleClient.Current.Homeport;
@@ -364,7 +365,7 @@ namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
 			this.ResultList = Results.ToList();
 
 			this.SortWorker = new ShipCatalogSortWorker();
-			this.SortWorker.SetTarget(ShipCatalogSortTarget.Level,true);
+			this.SortWorker.SetTarget(ShipCatalogSortTarget.Level, true);
 
 			this.updateSource
 				.Do(_ => this.IsReloading = true)
