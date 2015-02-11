77df0483511e17489ed140a2903ada4a34302a5a
FreyYa <hong09072@gmail.com>
2015-02-11 13:09:19 +0900

    Modify more safe

--- Plugins/CsvLogReader/ViewModels/LogViewerViewModel.cs
+++ Plugins/CsvLogReader/ViewModels/LogViewerViewModel.cs
@@ -1,11 +1,26 @@
 ﻿using Livet;
 using System.Diagnostics;
 using System.IO;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System;
 
 namespace LogReader.ViewModels
 {
 	public class LogViewerViewModel : ViewModel
 	{
+		bool isExecuting = false;
+		// FindWindow 사용을 위한 코드
+		[DllImport("user32.dll", CharSet = CharSet.Auto)]
+		public static extern IntPtr FindWindow(string strClassName, string StrWindowName);
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto)]
+		public static extern void SetForegroundWindow(IntPtr hWnd);
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto)]
+		public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+		private const int SW_SHOWNORMAL = 1;
 		//#region static members
 		//private static readonly LogViewerViewModel current = new LogViewerViewModel();
 
@@ -22,16 +37,38 @@ namespace LogReader.ViewModels
 		}
 		public void ShowLogViewer()
 		{
+			Process[] process = Process.GetProcesses();
 			string MainFolder = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
-			foreach (Process process in Process.GetProcesses())
+			foreach (Process proc in process)
 			{
-				if (process.MainWindowTitle == "제독업무도 바빠! 기록열람" && process.ProcessName.StartsWith("LogArchive"))
+
+				if (proc.ProcessName.Equals("LogArchive"))
+				//  Pgm_FileName 프로그램의 실행 파일[.exe]를 제외한 파일명
 				{
-					process.Kill();
+					isExecuting = true;
+					break;
+				}
+
+				else
+					isExecuting = false;
+			}
+			if (isExecuting)
+			{IntPtr procHandler = FindWindow(null, "제독업무도 바빠! 기록열람");
+				ShowWindow(procHandler, SW_SHOWNORMAL);
+				SetForegroundWindow(procHandler);
+			}
+			else if(!isExecuting)
+			{
+				if (File.Exists(Path.Combine(MainFolder, "LogArchive.exe")))
+				{
+					Process MyProcess = new Process();
+					MyProcess.StartInfo.FileName = "LogArchive.exe";
+					MyProcess.StartInfo.WorkingDirectory = MainFolder;
+					MyProcess.Start();
+					MyProcess.Refresh();
 				}
 			}
-			var Applocate = Path.Combine(MainFolder, "LogArchive.exe");
-			Process.Start(Applocate);
+
 		}
 		//public void ShowDropLogViewer()
 		//{
