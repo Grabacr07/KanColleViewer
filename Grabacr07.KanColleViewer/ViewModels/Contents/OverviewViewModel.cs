013ed71c7813bc6037d93028a56948997419dc33
FreyYa <hong09072@gmail.com>
2015-02-12 08:53:01 +0900

    Fix UI

--- Grabacr07.KanColleViewer/ViewModels/Contents/OverviewViewModel.cs
+++ Grabacr07.KanColleViewer/ViewModels/Contents/OverviewViewModel.cs
@@ -1,7 +1,10 @@
-﻿using Grabacr07.KanColleViewer.Properties;
+﻿using Grabacr07.KanColleViewer.Models;
+using Grabacr07.KanColleViewer.Properties;
 using Grabacr07.KanColleViewer.ViewModels.Catalogs;
 using Livet.Messaging;
 using System;
+using System.Windows;
+using Settings2 = Grabacr07.KanColleViewer.Models.Settings;
 
 namespace Grabacr07.KanColleViewer.ViewModels.Contents
 {
@@ -12,12 +15,93 @@ namespace Grabacr07.KanColleViewer.ViewModels.Contents
 			get { return Resources.IntegratedView; }
 			protected set { throw new NotImplementedException(); }
 		}
+		#region VerticalBar 変更通知プロパティ
+
+		private Visibility _VerticalBar;
+
+		public Visibility VerticalBar
+		{
+			get { return this._VerticalBar; }
+			set
+			{
+				if (this._VerticalBar != value)
+				{
+					this._VerticalBar = value;
+					this.RaisePropertyChanged();
+				}
+			}
+		}
+
+		#endregion
+
+		#region HorizontalBar 変更通知プロパティ
+
+		private Visibility _HorizontalBar;
+
+		public Visibility HorizontalBar
+		{
+			get { return this._HorizontalBar; }
+			set
+			{
+				if (this._HorizontalBar != value)
+				{
+					this._HorizontalBar = value;
+					this.RaisePropertyChanged();
+				}
+			}
+		}
+
+		#endregion
+
+		#region ThirdColumn 変更通知プロパティ
+
+		private double _ThirdColumn;
+
+		public double ThirdColumn
+		{
+			get { return this._ThirdColumn; }
+			set
+			{
+				if (this._ThirdColumn != value)
+				{
+					this._ThirdColumn = value;
+					this.RaisePropertyChanged();
+				}
+			}
+		}
+
+		#endregion
 
 		public MainContentViewModel Content { get; }
 
 
 		public OverviewViewModel(MainContentViewModel owner)
 		{
+			if (Settings2.Current.Orientation == OrientationType.Vertical)
+			{
+				this.VerticalBar = Visibility.Visible;
+				this.HorizontalBar = Visibility.Collapsed;
+				this.ThirdColumn = 205.0;
+			}
+			else
+			{
+				this.VerticalBar = Visibility.Collapsed;
+				this.HorizontalBar = Visibility.Visible;
+				this.ThirdColumn = 230.0;
+			}
+
+			Settings2.Current.VerticalWindow += () =>
+			{
+				this.VerticalBar = Visibility.Visible;
+				this.HorizontalBar = Visibility.Collapsed;
+				this.ThirdColumn = 205.0;
+			};
+			Settings2.Current.HorizontalWindow += () =>
+			{
+				this.VerticalBar = Visibility.Collapsed;
+				this.HorizontalBar = Visibility.Visible;
+				this.ThirdColumn = 230.0;
+			};
 			this.Content = owner;
 		}
 
