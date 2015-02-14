3950b76252ef216cfc0fcba1c3dad20fe1199edb
Grabacr07 <grabacr07+github@gmail.com>
2015-02-15 04:49:36 +0900

    護衛退避検出実装 (動作未確認)

--- Grabacr07.KanColleWrapper/Models/RepairingDock.cs
+++ Grabacr07.KanColleWrapper/Models/RepairingDock.cs
@@ -91,8 +91,8 @@ namespace Grabacr07.KanColleWrapper.Models
 				{
 					var oldShip = this.target;
 					var newShip = value;
-					if (oldShip != null) oldShip.IsInRepairing = false;
-					if (newShip != null) newShip.IsInRepairing = true;
+					if (oldShip != null) oldShip.Status ^= ShipStatus.Repair;
+					if (newShip != null) newShip.Status |= ShipStatus.Repair;
 
 					this.target = value;
 					this.RaisePropertyChanged();
