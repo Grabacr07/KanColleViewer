efd911290807692f4b3f8f433bf4c67d9d220825
FreyYa <hong09072@gmail.com>
2015-02-15 01:32:07 +0900

    modify adjustconverter

--- Grabacr07.KanColleViewer/Views/Converters/AdjustConverter.cs
+++ Grabacr07.KanColleViewer/Views/Converters/AdjustConverter.cs
@@ -7,23 +7,26 @@ using System.Threading.Tasks;
 using System.Windows.Data;
 using System.Windows.Media;
 using Grabacr07.KanColleWrapper.Models;
+using Settings2 = Grabacr07.KanColleViewer.Models.Settings;
 
 namespace Grabacr07.KanColleViewer.Views.Converters
 {
-    public class AdjustConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var number = Double.Parse(value.ToString());
-            var plus = Double.Parse(parameter.ToString());
-            return number + plus;
-        }
+	public class AdjustConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var number = Double.Parse(value.ToString());
+			var plus = Double.Parse(parameter.ToString());
+			if (Settings2.Current.OrientationMode == Models.OrientationType.Vertical) plus = plus - 54.00;
+			return number + plus;
+		}
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var number = Double.Parse(value.ToString());
-            var plus = Double.Parse(parameter.ToString());
-            return number - plus;
-        }
-    }
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			var number = Double.Parse(value.ToString());
+			var plus = Double.Parse(parameter.ToString());
+			if (Settings2.Current.OrientationMode == Models.OrientationType.Vertical) plus = plus - 54.00;
+			return number - plus;
+		}
+	}
 }
