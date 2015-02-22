96e6d72615d45a7b8a3a4217356152b3692d011c
FreyYa <hong09072@gmail.com>
2015-02-22 16:59:36 +0900

    fix flag check

--- Grabacr07.KanColleViewer/ViewModels/Catalogs/ShipCatalogFilter.cs
+++ Grabacr07.KanColleViewer/ViewModels/Catalogs/ShipCatalogFilter.cs
@@ -608,7 +608,7 @@ namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
 
 		public override bool Predicate(Ship ship)
 		{
-			if (ship.Situation == ShipSituation.Repair) return false;
+			if (ship.Situation.HasFlag(ShipSituation.Repair)) return false;
 			if (ship.RepairTime != 0) return true;
 			return false;
 		}
