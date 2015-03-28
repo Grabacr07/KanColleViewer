88d186b738d8a300e39f424c6224eba4e70566a1
Paddy Xu <xupaddy@gmail.com>
2015-03-28 21:03:36 +0100

    LogViewer plug-in

---
+++ Plugins/LogViewer/Controls/LogListView.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using Grabacr07.KanColleViewer.Plugins.ViewModels;
+
+namespace Grabacr07.KanColleViewer.Plugins.Controls
+{
+    public class LogListView
+    {
+        public static readonly DependencyProperty LogSourceProperty =
+            DependencyProperty.RegisterAttached("LogSource",
+                                                typeof(LogItemCollection), typeof(LogListView),
+                                                new FrameworkPropertyMetadata(null, OnLogSourceChanged));
+
+        public static LogItemCollection GetLogSource(DependencyObject d)
+        {
+            return (LogItemCollection)d.GetValue(LogSourceProperty);
+        }
+
+        public static void SetLogSource(DependencyObject d, LogItemCollection value)
+        {
+            d.SetValue(LogSourceProperty, value);
+        }
+
+        private static void OnLogSourceChanged(DependencyObject d,
+            DependencyPropertyChangedEventArgs e)
+        {
+            ListView listView = d as ListView;
+            LogItemCollection collection = e.NewValue as LogItemCollection;
+
+            listView.ItemsSource = collection;
+            GridView gridView = listView.View as GridView;
+            int count = 0;
+            gridView.Columns.Clear();
+            foreach (var col in collection.Columns)
+            {
+                var cell = new FrameworkElementFactory(typeof(TextBlock));
+                cell.SetBinding(TextBlock.TextProperty, new Binding(string.Format("[{0}]", count++)));
+                cell.SetValue(FrameworkElement.MarginProperty, new Thickness(0, 4, 0, 4));
+
+                gridView.Columns.Add(
+                    new GridViewColumn
+                    {
+                        Header = new TextBlock { Text = col, FontSize = 11, Margin = new Thickness(5, 4, 5, 4) },
+                        CellTemplate = new DataTemplate
+                        {
+                            DataType = typeof(LogItemCollection),
+                            VisualTree = cell,
+                        }
+                    });
+            }
+        }
+    }
+}
---
+++ Plugins/LogViewer/ViewModels/LogItemCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grabacr07.KanColleViewer.Plugins.ViewModels
+{
+    public class LogItemCollection : IEnumerable
+    {
+        public List<string> Columns = new List<string>();
+        public List<string[]> Rows = new List<string[]>();
+
+        public IEnumerator GetEnumerator()
+        {
+            return this.Rows.GetEnumerator();
+        }
+    }
+}
