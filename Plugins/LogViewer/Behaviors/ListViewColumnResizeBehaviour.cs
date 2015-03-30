481d6d91b0aabe717a73a6b79697d9c746a1bfb8
Paddy Xu <xupaddy@gmail.com>
2015-03-30 10:14:26 +0200

    Fix: ListView column does not resize when items changed in background

---
+++ Plugins/LogViewer/Behaviors/ListViewColumnResizeBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace Grabacr07.KanColleViewer.Plugins.Behaviors
+{
+    public class IsVisibleChangedBehaviour : Behavior<ListView>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.IsVisibleChanged += this.AssociatedObject_IsVisibleChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            this.AssociatedObject.IsVisibleChanged -= this.AssociatedObject_IsVisibleChanged;
+            base.OnDetaching();
+        }
+
+        private void AssociatedObject_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            GridView gv = this.AssociatedObject.View as GridView;
+            if (gv != null)
+            {
+                foreach (var c in gv.Columns)
+                {
+                    // Code below was found in GridViewColumnHeader.OnGripperDoubleClicked() event handler (using Reflector)
+                    // i.e. it is the same code that is executed when the gripper is double clicked
+                    if (double.IsNaN(c.Width))
+                    {
+                        c.Width = c.ActualWidth;
+                    }
+                    c.Width = double.NaN;
+                }
+            }
+        }
+    }
+}
