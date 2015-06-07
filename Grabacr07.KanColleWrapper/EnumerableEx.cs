51b0183d19ce5b8f3578a34277979a6266ee039c
Grabacr07 <grabacr07+github@gmail.com>
2015-06-07 19:35:00 +0900

    整理

--- Grabacr07.KanColleWrapper/EnumerableEx.cs
+++ Grabacr07.KanColleWrapper/EnumerableEx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Grabacr07.KanColleWrapper
 {
@@ -19,6 +20,26 @@ namespace Grabacr07.KanColleWrapper
 				var key = keySelector(item);
 				if (set.Add(key)) yield return item;
 			}
+		}
+
+		/// <summary>
+		/// コレクションを展開し、メンバーの文字列表現を指定した区切り文字で連結した文字列を返します。
+		/// </summary>
+		/// <typeparam name="T">コレクションに含まれる任意の型。</typeparam>
+		/// <param name="source">対象のコレクション。</param>
+		/// <param name="separator">セパレーターとして使用する文字列。</param>
+		/// <returns>コレクションの文字列表現を展開し、指定したセパレーターで連結した文字列。</returns>
+		public static string ToString<T>(this IEnumerable<T> source, string separator)
+		{
+			return string.Join(separator, source);
+		}
+
+		/// <summary>
+		/// シーケンスが null でなく、1 つ以上の要素を含んでいるかどうかを確認します。
+		/// </summary>
+		public static bool HasItems<T>(this IEnumerable<T> source)
+		{
+			return source != null && source.Any();
 		}
 	}
 }
