ca789b145fb537b5d5df07b5275dccc2324b45e2
FreyYa <hong09072@gmail.com>
2015-06-08 20:04:00 +0900

    Make PerHour Material Calc

--- Grabacr07.KanColleViewer/ViewModels/Catalogs/ExpeditionsCatalogWindowViewModel.cs
+++ Grabacr07.KanColleViewer/ViewModels/Catalogs/ExpeditionsCatalogWindowViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Grabacr07.KanColleWrapper;
+using Grabacr07.KanColleWrapper.Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -17,11 +19,23 @@ namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
 				if (this._ExpeditionList != value)
 				{
 					this._ExpeditionList = value;
-					if (this._ExpeditionList.Count == this.ListCount)
-						this.RaisePropertyChanged();
+					this.RaisePropertyChanged();
 				}
 			}
 		}
+		private bool _IsPerHour;
+		public bool IsPerHour
+		{
+			get { return this._IsPerHour; }
+			set
+			{
+				if (this._IsPerHour == value) return;
+				this._IsPerHour = value;
+				this.Update(value);
+				this.RaisePropertyChanged();
+			}
+		}
+
 		private int ListCount { get; set; }
 		public ExpeditionsCatalogWindowViewModel()
 		{
@@ -29,9 +43,14 @@ namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
 			this.ListCount = 0;
 			this.Update();
 		}
-		private void Update()
+		private decimal ConvertToDecimal(string context)
 		{
-			this.ExpeditionList = new List<ExpeditionData>();
+			if (context == string.Empty) return 0;
+			return Convert.ToDecimal(context);
+		}
+		private void Update(bool IsChecked = false)
+		{
+			var tempList = new List<ExpeditionData>();
 			ListCount = KanColleClient.Current.Translations.GetExpeditionListCount();
 
 			bool IsEnd = true;
@@ -40,22 +59,18 @@ namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
 			{
 				ExpeditionData temp = new ExpeditionData
 				{
-					ID = i.ToString(),
+					ID = i,
 					TRName = KanColleClient.Current.Translations.GetExpeditionData("TR-Name", i),
 					FlagLv = KanColleClient.Current.Translations.GetExpeditionData("FlagLv", i),
 					NeedShip = KanColleClient.Current.Translations.GetExpeditionData("NeedShip", i),
 					Time = KanColleClient.Current.Translations.GetExpeditionData("Time", i),
-					Fuel = KanColleClient.Current.Translations.GetExpeditionData("Fuel", i),
-					Armo = KanColleClient.Current.Translations.GetExpeditionData("Armo", i),
-					Metal = KanColleClient.Current.Translations.GetExpeditionData("Metal", i),
-					Bo = KanColleClient.Current.Translations.GetExpeditionData("Bo", i),
+					Fuel = ConvertToDecimal(KanColleClient.Current.Translations.GetExpeditionData("Fuel", i)),
+					Armo = ConvertToDecimal(KanColleClient.Current.Translations.GetExpeditionData("Armo", i)),
+					Metal = ConvertToDecimal(KanColleClient.Current.Translations.GetExpeditionData("Metal", i)),
+					Bo = ConvertToDecimal(KanColleClient.Current.Translations.GetExpeditionData("Bo", i)),
 					Detail = KanColleClient.Current.Translations.GetExpeditionData("Detail", i),
 				};
-				if (temp.FlagLv != string.Empty && temp.FlagLv != "-")
-				{
-					temp.IntLv = Convert.ToInt32(temp.FlagLv);
-					temp.FlagLv = "Lv. " + temp.FlagLv;
-				}
+				if (temp.FlagLv != string.Empty && temp.FlagLv != "-") temp.FlagLv = "Lv. " + temp.FlagLv;
 				if (temp.Time != string.Empty)
 				{
 					var splitTime = temp.Time.Split(';');
@@ -75,176 +90,39 @@ namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
 					if (Time.Hour != 0) timeString.Append(Time.Hour + "시간");
 					if (Time.Minute != 0) timeString.Append(Time.Minute + "분");
 					temp.Time = timeString.ToString();
+					if (IsChecked)
+					{
+						decimal PerHour = Convert.ToDecimal((temp.RealTime.Day - 1) * 24 * 60 + temp.RealTime.Hour * 60 + temp.RealTime.Minute) / 60m;
+						if (temp.Fuel > 0)
+							temp.Fuel = Math.Round(temp.Fuel / PerHour, 1);
+						if (temp.Armo > 0)
+							temp.Armo = Math.Round(temp.Armo / PerHour, 1);
+						if (temp.Metal > 0)
+							temp.Metal = Math.Round(temp.Metal / PerHour, 1);
+						if (temp.Bo > 0)
+							temp.Bo = Math.Round(temp.Bo / PerHour, 1);
+					}
+
 				}
 				i++;
-				if (temp.TRName != string.Empty && temp.FlagLv != string.Empty) this.ExpeditionList.Add(temp);
-				if (this.ExpeditionList.Count == this.ListCount) IsEnd = false;
+				if (temp.TRName != string.Empty && temp.FlagLv != string.Empty) tempList.Add(temp);
+				if (tempList.Count == this.ListCount) IsEnd = false;
 			}
+			this.ExpeditionList = new List<ExpeditionData>(tempList);
 		}
 	}
 	public class ExpeditionData
 	{
-
-		#region ID変更通知プロパティ
-		private string _ID;
-
-		public string ID
-		{
-			get
-			{ return this._ID; }
-			set
-			{
-				if (this._ID == value)
-					return;
-				this._ID = value;
-			}
-		}
-		#endregion
-
-		#region TRName変更通知プロパティ
-		private string _TRName;
-
-		public string TRName
-		{
-			get
-			{ return this._TRName; }
-			set
-			{
-				if (this._TRName == value)
-					return;
-				this._TRName = value;
-			}
-		}
-		#endregion
-
-		#region FlagLv変更通知プロパティ
-		private string _FlagLv;
-		public int IntLv { get; set; }
-
-		public string FlagLv
-		{
-			get
-			{ return this._FlagLv; }
-			set
-			{
-				if (this._FlagLv == value)
-					return;
-				this._FlagLv = value;
-			}
-		}
-		#endregion
-
-		#region NeedShip変更通知プロパティ
-		private string _NeedShip;
-
-		public string NeedShip
-		{
-			get
-			{ return this._NeedShip; }
-			set
-			{
-				if (this._NeedShip == value)
-					return;
-				this._NeedShip = value;
-			}
-		}
-		#endregion
-
-		#region Time変更通知プロパティ
-		private string _Time;
+		public int ID { get; set; }
+		public string TRName { get; set; }
+		public string FlagLv { get; set; }
+		public string NeedShip { get; set; }
+		public string Time { get; set; }
 		public DateTime RealTime { get; set; }
-		public string Time
-		{
-			get
-			{ return this._Time; }
-			set
-			{
-				if (this._Time == value)
-					return;
-				this._Time = value;
-			}
-		}
-		#endregion
-
-		#region Fuel変更通知プロパティ
-		private string _Fuel;
-
-		public string Fuel
-		{
-			get
-			{ return this._Fuel; }
-			set
-			{
-				if (this._Fuel == value)
-					return;
-				this._Fuel = value;
-			}
-		}
-		#endregion
-
-		#region Armo変更通知プロパティ
-		private string _Armo;
-
-		public string Armo
-		{
-			get
-			{ return this._Armo; }
-			set
-			{
-				if (this._Armo == value)
-					return;
-				this._Armo = value;
-			}
-		}
-		#endregion
-
-		#region Metal変更通知プロパティ
-		private string _Metal;
-
-		public string Metal
-		{
-			get
-			{ return this._Metal; }
-			set
-			{
-				if (this._Metal == value)
-					return;
-				this._Metal = value;
-			}
-		}
-		#endregion
-
-		#region Bo変更通知プロパティ
-		private string _Bo;
-
-		public string Bo
-		{
-			get
-			{ return this._Bo; }
-			set
-			{
-				if (this._Bo == value)
-					return;
-				this._Bo = value;
-			}
-		}
-		#endregion
-
-		#region Detail変更通知プロパティ
-		private string _Detail;
-
-		public string Detail
-		{
-			get
-			{ return this._Detail; }
-			set
-			{
-				if (this._Detail == value)
-					return;
-				this._Detail = value;
-			}
-		}
-		#endregion
-
+		public decimal Fuel { get; set; }
+		public decimal Armo { get; set; }
+		public decimal Metal { get; set; }
+		public decimal Bo { get; set; }
+		public string Detail { get; set; }
 	}
 }
