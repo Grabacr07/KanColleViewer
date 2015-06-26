b8dd8e0ff45b5d8707b25ed5fa0828c86431b9d2
Grabacr07 <grabacr07+github@gmail.com>
2015-06-26 17:12:44 +0900

    IsDefault とか IsCancel とかあるじゃろ

---
+++ Grabacr07.KanColleViewer/Views/Behaviors/FocusAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace Grabacr07.KanColleViewer.Views.Behaviors
+{
+	public class FocusAction : TriggerAction<UIElement>
+	{
+		protected override void Invoke(object parameter)
+		{
+			if (this.AssociatedObject.Focusable)
+			{
+				this.AssociatedObject.Focus();
+			}
+		}
+	}
+}
