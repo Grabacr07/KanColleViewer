77f7c48e6fe11e18cdea71cca69f3c7bb679088d
Grabacr07 <grabacr07+github@gmail.com>
2015-07-09 12:58:44 +0900

    source に移動

---
+++ source/Plugins/Counter/Counters.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Grabacr07.KanColleWrapper;
+using Grabacr07.KanColleWrapper.Models.Raw;
+using Livet;
+
+namespace Counter
+{
+	public abstract class CounterBase : NotificationObject
+	{
+		#region Text 変更通知プロパティ
+
+		private string _Text;
+
+		public string Text
+		{
+			get { return this._Text; }
+			set
+			{
+				if (this._Text != value)
+				{
+					this._Text = value;
+					this.RaisePropertyChanged();
+				}
+			}
+		}
+
+		#endregion
+
+		#region Count 変更通知プロパティ
+
+		private int _Count;
+
+		public int Count
+		{
+			get { return this._Count; }
+			set
+			{
+				if (this._Count != value)
+				{
+					this._Count = value;
+					this.RaisePropertyChanged();
+				}
+			}
+		}
+
+		#endregion
+
+		public void Reset()
+		{
+			this.Count = 0;
+		}
+	}
+
+	public class ItemDestroyCounter : CounterBase
+	{
+		public ItemDestroyCounter(KanColleProxy proxy)
+		{
+			proxy.api_req_kousyou_destroyitem2
+				.TryParse()
+				.Where(x => x.IsSuccess)
+				.Subscribe(_ => this.Count++);
+
+			this.Text = "装備を破棄した回数";
+		}
+	}
+
+	public class SupplyCounter : CounterBase
+	{
+		public SupplyCounter(KanColleProxy proxy)
+		{
+			proxy.api_req_hokyu_charge
+				.TryParse()
+				.Where(x => x.IsSuccess)
+				.Subscribe(_ => this.Count++);
+
+			this.Text = "艦娘に補給した回数";
+		}
+	}
+
+
+	public class MissionCounter : CounterBase
+	{
+		public MissionCounter(KanColleProxy proxy)
+		{
+			proxy.api_req_mission_result
+				.TryParse<kcsapi_mission_result>()
+				.Where(x => x.IsSuccess)
+				.Where(x => x.Data.api_clear_result == 1 || x.Data.api_clear_result == 2)
+				.Subscribe(_ => this.Count++);
+
+			this.Text = "遠征に成功した回数";
+		}
+	}
+}
---
+++ source/Plugins/WindowsNotifier/Toast.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using DesktopToast;
+
+namespace Grabacr07.KanColleViewer.Plugins
+{
+	/// <summary>
+	/// Windows 8 のトースト通知機能を提供します。
+	/// </summary>
+	public class Toast
+	{
+		#region static members
+
+		/// <summary>
+		/// トースト通期機能をサポートしているかどうかを示す値を取得します。
+		/// </summary>
+		/// <returns>
+		/// 動作しているオペレーティング システムが Windows 8 (NT 6.2) 以降の場合は true、それ以外の場合は false。
+		/// </returns>
+		public static bool IsSupported
+		{
+			get
+			{
+				var version = Environment.OSVersion.Version;
+				return (version.Major == 6 && version.Minor >= 2) || version.Major > 6;
+
+			}
+		}
+
+		#endregion
+#if DEBUG
+		public const string AppId = "Grabacr07.KanColleViewer.Debug";
+#else
+		public const string AppId = "Grabacr07.KanColleViewer"; 
+#endif
+
+		public event Action Activated;
+
+		public event Action ToastFailed;
+
+		private readonly ToastRequest request;
+
+		public Toast(string header, string body)
+		{
+			this.request = new ToastRequest
+			{
+				ToastHeadline = header,
+				ToastBody = body,
+#if DEBUG
+				ShortcutFileName = "提督業も忙しい！ (debug).lnk",
+#else
+				ShortcutFileName = "提督業も忙しい！.lnk", 
+#endif
+				ShortcutTargetFilePath = Assembly.GetEntryAssembly().Location,
+				AppId = AppId,
+			};
+		}
+
+		public void Show()
+		{
+			ToastManager.ShowAsync(this.request)
+				.ContinueWith(t =>
+				{
+					if (t.Result == ToastResult.Activated)
+						this.Activated?.Invoke();
+					if (t.Result == ToastResult.Failed)
+						this.ToastFailed?.Invoke();
+				});
+		}
+	}
+}
