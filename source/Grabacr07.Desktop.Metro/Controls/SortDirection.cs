5d5bcec50d0e8c555f7da51fb9d35ea87434ad28
Grabacr07 <grabacr07+github@gmail.com>
2015-07-09 12:54:51 +0900

    source に移動

---
+++ source/Grabacr07.Desktop.Metro/Controls/SortDirection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grabacr07.Desktop.Metro.Controls
+{
+	public enum SortDirection
+	{
+		/// <summary>
+		/// ソート対象ではありません。
+		/// </summary>
+		None,
+
+		Ascending,
+		Descending,
+	}
+}
