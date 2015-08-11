8dd170ee8031aa7453952ee943ebfb04e2a49c6e
Grabacr07 <grabacr07+github@gmail.com>
2015-08-12 02:34:51 +0900

    v4.1

--- source/Grabacr07.KanColleWrapper/Properties/AssemblyInfo.cs
+++ source/Grabacr07.KanColleWrapper/Properties/AssemblyInfo.cs
@@ -25,4 +25,4 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.2.1.0")]
