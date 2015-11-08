837ed35b2278c3502c8501965ca185ebc8c8f4dc
Yuubari <remilia@koumakan.jp>
2015-11-08 15:27:18 +0300

    Implement repair timer tool-tips

--- source/Grabacr07.KanColleViewer/ViewModels/Contents/ShipViewModel.cs
+++ source/Grabacr07.KanColleViewer/ViewModels/Contents/ShipViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Grabacr07.KanColleViewer.Properties;
 using Grabacr07.KanColleWrapper.Models;
 using Livet;
 
@@ -10,6 +11,14 @@ namespace Grabacr07.KanColleViewer.ViewModels.Contents
 	public class ShipViewModel : ViewModel
 	{
 		public Ship Ship { get; }
+
+		public string RepairToolTip => 
+			!((this.Ship.HP.Maximum - this.Ship.HP.Current) > 0) ? 
+				"OK" : 
+				string.Format(Resources.Ship_RepairDockToolTip, this.Ship.RepairTimeDock) 
+				+ ((((this.Ship.HP.Current / (double)this.Ship.HP.Maximum) > 0.5) && this.Ship.RepairTimeFacility != this.Ship.RepairTimeDock) ? 
+					"\n" + string.Format(Resources.Ship_RepairFacilityToolTip, this.Ship.RepairTimeFacility) : 
+					"");
 
 		public ShipViewModel(Ship ship)
 		{
