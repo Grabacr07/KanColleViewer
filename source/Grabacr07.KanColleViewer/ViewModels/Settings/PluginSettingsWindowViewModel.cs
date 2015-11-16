4b4ffc972b2cbc76ed4e4164974e17cad4690a37
Yuubari <remilia@koumakan.jp>
2015-11-16 19:00:42 +0300

    Let plug-ins specify settings window size

--- source/Grabacr07.KanColleViewer/ViewModels/Settings/PluginSettingsWindowViewModel.cs
+++ source/Grabacr07.KanColleViewer/ViewModels/Settings/PluginSettingsWindowViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using Grabacr07.KanColleViewer.Composition;
 using Grabacr07.KanColleViewer.Controls.Globalization;
 using MetroTrilithon.Mvvm;
@@ -13,12 +15,35 @@ namespace Grabacr07.KanColleViewer.ViewModels.Settings
 	{
 		private readonly ISettings settings;
 
+		private readonly Size defaultSize = new Size(400, 350);
+
 		public object Content => this.settings.View;
+
+		public double Height
+		{
+			get { return GetSize().Height; }
+			set { }
+		}
+
+		public double Width
+		{
+			get { return GetSize().Width; }
+			set { }
+		}
 
 		public PluginSettingsWindowViewModel(ISettings settings, string title)
 		{
 			this.Title = string.Format(KanColleViewer.Properties.Resources.Settings_Plugins_PluginSettingsTitle, title);
 			this.settings = settings;
+		}
+
+		private Size GetSize()
+		{
+			PropertyInfo pInfo = settings.GetType().GetProperty("SettingsSize");
+
+			if ((pInfo == null) || (pInfo.PropertyType != typeof(Size))) return defaultSize;
+
+			return (Size)pInfo.GetValue(settings);
 		}
 	}
 }
