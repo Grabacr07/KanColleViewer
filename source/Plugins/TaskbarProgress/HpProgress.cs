0d1309c73539b579aef392fb5f1f63ec7a667765
veigr <veigr@cat-ears.net>
2016-02-08 01:14:39 +0900

    コード微修正

--- source/Plugins/TaskbarProgress/HpProgress.cs
+++ source/Plugins/TaskbarProgress/HpProgress.cs
@@ -19,7 +19,7 @@ namespace Grabacr07.KanColleViewer.Plugins
 	[Export(typeof(ITaskbarProgress))]
 	[ExportMetadata("Guid", guid)]
 	[ExportMetadata("Title", "HpProgressIndicator")]
-	[ExportMetadata("Description", "艦隊内の最大損害艦耐久をタスク バー インジケーターに報告します。")]
+	[ExportMetadata("Description", "艦隊内の最大損害艦 HP をタスク バー インジケーターに報告します。")]
 	[ExportMetadata("Version", "1.0")]
 	[ExportMetadata("Author", "@veigr")]
 	public class HpProgress : IPlugin, ITaskbarProgress, IDisposableHolder
@@ -27,12 +27,12 @@ namespace Grabacr07.KanColleViewer.Plugins
 		private const string guid = "DA0E7091-F4A6-4467-9812-3C3E0DF946EA";
 
 		private readonly MultipleDisposable compositDisposable = new MultipleDisposable();
-		private List<IDisposable> fleetHandlers = new List<IDisposable>();
+		private readonly List<IDisposable> fleetHandlers = new List<IDisposable>();
 		private bool initialized;
 
 		public string Id => guid + "-1";
 
-		public string DisplayName => "艦隊内の最大損害艦耐久";
+		public string DisplayName => "艦隊内の最大損害艦 HP";
 
 		public TaskbarItemProgressState State { get; private set; }
 
@@ -52,50 +52,47 @@ namespace Grabacr07.KanColleViewer.Plugins
 			if (this.initialized) return;
 
 			var homeport = KanColleClient.Current.Homeport;
-			if (homeport != null)
-			{
-				this.initialized = true;
-				homeport.Organization
-					.Subscribe(nameof(Organization.Fleets), this.UpdateFleets)
-					.AddTo(this);
-			}
+			if (homeport == null) return;
+
+			this.initialized = true;
+			homeport.Organization
+				.Subscribe(nameof(Organization.Fleets), this.UpdateFleets)
+				.AddTo(this);
 		}
 
 		public void UpdateFleets()
 		{
 			if (!this.initialized) return;
 
-			foreach (var handler in fleetHandlers)
+			foreach (var handler in this.fleetHandlers)
 			{
 				handler.Dispose();
 			}
-			fleetHandlers.Clear();
+			this.fleetHandlers.Clear();
 
-			KanColleClient.Current.Homeport.Organization.Fleets.Values
-				.Select(x => x.Subscribe(nameof(Fleet.Ships), this.Update))
-				.Do(x => fleetHandlers.Add(x))
-				.ToArray();
-			KanColleClient.Current.Homeport.Organization.Fleets.Values
-				.Select(x => x.Subscribe(nameof(Fleet.IsInSortie), this.Update))
-				.Do(x => fleetHandlers.Add(x))
-				.ToArray();
+			foreach (var fleet in KanColleClient.Current.Homeport.Organization.Fleets.Values)
+			{
+				this.fleetHandlers.Add(fleet.Subscribe(nameof(Fleet.Ships), this.Update));
+				this.fleetHandlers.Add(fleet.Subscribe(nameof(Fleet.IsInSortie), this.Update));
+			}
 		}
 
 		public void Update()
 		{
 			var org = KanColleClient.Current.Homeport.Organization;
-			IEnumerable<Ship> ships;
+			Ship[] ships;
 			if (org.Fleets.Values.Any(x => x.IsInSortie))
 			{
 				ships = org.Fleets.Values
 					.Where(x => x.IsInSortie)
-					.SelectMany(x => x.Ships);
+					.SelectMany(x => x.Ships)
+					.ToArray();
 			}
 			else
 			{
 				ships = org.Combined
-					? org.CombinedFleet.Fleets.SelectMany(x => x.Ships)
-					: org.Fleets[1].Ships;
+					? org.CombinedFleet.Fleets.SelectMany(x => x.Ships).ToArray()
+					: org.Fleets[1].Ships.ToArray();
 			}
 
 			if (!ships.Any())
