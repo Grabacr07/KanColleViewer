4492aacc9b0b886dd81ef5e4c5346fb232a5c5f5
Manato KAMEYA <grabacr07+github@gmail.com>
2016-04-01 20:33:08 +0900

    4 月 1 日アップデート対応

--- source/Grabacr07.KanColleWrapper/KanColleProxy.Endpoints.cs
+++ source/Grabacr07.KanColleWrapper/KanColleProxy.Endpoints.cs
@@ -350,5 +350,13 @@ namespace Grabacr07.KanColleWrapper
 			get { return this.ApiSessionSource.Where(x => x.Request.PathAndQuery == "/kcsapi/api_req_combined_battle/goback_port"); }
 		}
 
+		/// <summary>
+		/// エンド ポイント "/kcsapi/api_get_member/require_info" からのセッションを配信します。
+		/// </summary>
+		public IObservable<Session> api_get_member_require_info
+		{
+			get { return this.ApiSessionSource.Where(x => x.Request.PathAndQuery == "/kcsapi/api_get_member/require_info"); }
+		}
+
 	}
 }
