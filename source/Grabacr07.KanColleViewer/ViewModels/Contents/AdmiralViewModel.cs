c6c510d0a15228f8b6ff6400196bca18056b3ab3
Cirno V <cirnov59@gmail.com>
2016-04-17 00:08:25 +0900

    전과 표시 기능 추가 (ElectronicObserver의 소스 차용: https://github.com/andanteyk/ElectronicObserver)

--- source/Grabacr07.KanColleViewer/ViewModels/Contents/AdmiralViewModel.cs
+++ source/Grabacr07.KanColleViewer/ViewModels/Contents/AdmiralViewModel.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Grabacr07.KanColleWrapper;
 using Grabacr07.KanColleWrapper.Models;
+using Grabacr07.KanColleViewer.Models;
 using Livet;
 using Livet.EventListeners;
+using System.Text;
 
 namespace Grabacr07.KanColleViewer.ViewModels.Contents
 {
@@ -16,17 +18,59 @@ namespace Grabacr07.KanColleViewer.ViewModels.Contents
 		public Admiral Model => KanColleClient.Current.Homeport.Admiral;
 
 		#endregion
+		
+		#region ToolTip 변경 통지 프로퍼티
+		private string _ToolTip;
+
+		public string ToolTip
+		{
+			get
+			{ return this._ToolTip; }
+			set
+			{
+				if (this._ToolTip == value)
+					return;
+				this._ToolTip = value;
+				this.RaisePropertyChanged();
+			}
+		}
+		#endregion
+
+		public static HQRecord Record;
 
 		public AdmiralViewModel()
 		{
+			Record = new HQRecord();
+			Record.Load();
 			this.CompositeDisposable.Add(new PropertyChangedEventListener(KanColleClient.Current.Homeport)
 			{
 				{ nameof(Homeport.Admiral), (sender, args) => this.Update() },
+				{ nameof(Homeport.Admiral), (sender, args) => Record.Updated() },
 			});
 		}
 
 		private void Update()
 		{
+			StringBuilder tooltip = new StringBuilder();
+			var res = Record.GetRecordPrevious();
+			if (res != null)
+			{
+				int diff = Model.Experience - res.HQExp;
+				tooltip.AppendFormat("이번: +{0} exp. / 전과 {1:n2}\r\n", diff, diff * 7 / 10000.0);
+			}
+			res = Record.GetRecordDay();
+			if (res != null)
+			{
+				int diff = Model.Experience - res.HQExp;
+				tooltip.AppendFormat("오늘: +{0} exp. / 전과 {1:n2}\r\n", diff, diff * 7 / 10000.0);
+			}
+			res = Record.GetRecordMonth();
+			if (res != null)
+			{
+				int diff = Model.Experience - res.HQExp;
+				tooltip.AppendFormat("이달: +{0} exp. / 전과 {1:n2}\r\n", diff, diff * 7 / 10000.0);
+			}
+			ToolTip = tooltip.ToString();
 			this.RaisePropertyChanged(nameof(this.Model));
 		}
 	}
