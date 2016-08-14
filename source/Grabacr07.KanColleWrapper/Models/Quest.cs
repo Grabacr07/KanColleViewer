fc63c37ddcfde4a4381744625d37659596bebb97
WolfgangKurz <wolfgangkurzdev@gmail.com>
2016-08-15 07:27:55 +0900

    수동 Merge. 원본 커밋들: - Grabacr07 프로젝트 06e90a8 커밋 - Grabacr07 프로젝트 c3d1204 커밋 - Grabacr07 프로젝트 6c3c663 커밋 - Grabacr07 프로젝트 8c38d18 커밋

--- source/Grabacr07.KanColleWrapper/Models/Quest.cs
+++ source/Grabacr07.KanColleWrapper/Models/Quest.cs
@@ -45,7 +45,8 @@ namespace Grabacr07.KanColleWrapper.Models
 		/// <summary>
 		/// 任務の詳細を取得します。
 		/// </summary>
-		public string Detail => KanColleClient.Current.Translations.GetTranslation(RawData.api_detail, TranslationType.QuestDetail, false, this.RawData, RawData.api_no);
+		public string Detail => KanColleClient.Current.Translations.GetTranslation(RawData.api_detail, TranslationType.QuestDetail, false, this.RawData, RawData.api_no)
+                                    .Replace("<br>", Environment.NewLine);
 
         /// <summary>
         /// 임무 화면에서의 페이지 번호
