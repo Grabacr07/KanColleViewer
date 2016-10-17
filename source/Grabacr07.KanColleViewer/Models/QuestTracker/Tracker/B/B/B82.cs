97a586ce6ddd75ca36dfec41bc65fdde2cf4e9eb
WolfgangKurz <wolfgangkurzdev@gmail.com>
2016-10-17 23:30:30 +0900

    출격 임무 추가 완료

---
+++ source/Grabacr07.KanColleViewer/Models/QuestTracker/Tracker/B/B/B82.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grabacr07.KanColleWrapper;
+using Grabacr07.KanColleWrapper.Models;
+using Grabacr07.KanColleViewer.Models.QuestTracker.Extensions;
+
+namespace Grabacr07.KanColleViewer.Models.QuestTracker.Tracker
+{
+	/// <summary>
+	/// 밤 바다를 비추는 불을 입수해라!
+	/// </summary>
+	internal class B82 : ITracker
+	{
+		private readonly int max_count = 1;
+		private int count;
+
+		public event EventHandler ProcessChanged;
+
+		int ITracker.Id => 830;
+		public QuestType Type => QuestType.OneTime;
+		public bool IsTracking { get; set; }
+
+		private System.EventArgs emptyEventArgs = new System.EventArgs();
+
+		public void RegisterEvent(TrackManager manager)
+		{
+			manager.BattleResultEvent += (sender, args) =>
+			{
+				if (!IsTracking) return;
+
+				if (args.MapWorldId != 2 || args.MapAreaId != 1) return; // 2-1
+				if (args.EnemyName != "敵主力艦隊") return; // boss
+				if ("S" == args.Rank) return; // S승리
+
+				count = count.Add(1).Max(max_count);
+
+				ProcessChanged?.Invoke(this, emptyEventArgs);
+			};
+		}
+
+		public void ResetQuest()
+		{
+			count = 0;
+			ProcessChanged?.Invoke(this, emptyEventArgs);
+		}
+
+		public int GetProgress()
+		{
+			return count * 100 / max_count;
+		}
+
+		public string GetProgressText()
+		{
+			return count >= max_count ? "완료" : "2-1 보스전 S승리 " + count.ToString() + " / " + max_count.ToString();
+		}
+
+		public string SerializeData()
+		{
+			return count.ToString();
+		}
+
+		public void DeserializeData(string data)
+		{
+			count = 0;
+			int.TryParse(data, out count);
+		}
+	}
+}
