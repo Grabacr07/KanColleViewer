e10178cb301766b4b2e522c64e1743b3446a73c8
WolfgangKurz <wolfgangkurzdev@gmail.com>
2016-10-17 21:03:50 +0900

    ~B50

---
+++ source/Grabacr07.KanColleViewer/Models/QuestTracker/Tracker/B/B/B45.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grabacr07.KanColleWrapper;
+using Grabacr07.KanColleWrapper.Models;
+using Grabacr07.KanColleViewer.Models.QuestTracker.Extensions;
+
+namespace Grabacr07.KanColleViewer.Models.QuestTracker.Tracker
+{
+	/// <summary>
+	/// 제6구축대 대잠초계가 철저한거에요!
+	/// </summary>
+	internal class B45 : ITracker
+	{
+		private readonly int max_count = 1;
+		private int count;
+
+		public event EventHandler ProcessChanged;
+
+		int ITracker.Id => 277;
+		public QuestType Type => QuestType.OneTime;
+		public bool IsTracking { get; set; }
+
+		private System.EventArgs emptyEventArgs = new System.EventArgs();
+
+		public void RegisterEvent(TrackManager manager)
+		{
+			manager.BattleResultEvent += (sender, args) =>
+			{
+				if (!IsTracking) return;
+
+				if (args.MapWorldId != 1 && args.MapAreaId != 5) return; // 1-5
+				if (args.EnemyName != "敵通商破壊主力艦隊") return; // boss
+				if (!"SA".Contains(args.Rank)) return; // A승 이상
+
+				var shipTable = new int[]
+				{
+					437, // 暁改二
+					35,  // 響
+					36,  // 雷
+					37,  // 電
+					147, // Верный
+					235, // 響改
+					236, // 雷改
+					237, // 電改
+				};
+
+				var fleet = KanColleClient.Current.Homeport.Organization.Fleets.FirstOrDefault(x => x.Value.IsInSortie).Value;
+				var ships = fleet.Ships;
+				if (ships.Length != 4) return;
+				if (ships[0].Info.Id != 437) return; // 기함 아카츠키改2
+				if (ships.Count(x => shipTable.Contains(x.Info.Id)) < 4) return;
+
+				count = count.Add(1).Max(max_count);
+
+				ProcessChanged?.Invoke(this, emptyEventArgs);
+			};
+		}
+
+		public void ResetQuest()
+		{
+			count = 0;
+			ProcessChanged?.Invoke(this, emptyEventArgs);
+		}
+
+		public int GetProgress()
+		{
+			return count * 100 / max_count;
+		}
+
+		public string GetProgressText()
+		{
+			return count >= max_count ? "완료" : "아카츠키改2 기함,히비키,이카즈치,이나즈마 만으로 편성 1-5 보스전 A승리 이상 " + count.ToString() + " / " + max_count.ToString();
+		}
+
+		public string SerializeData()
+		{
+			return count.ToString();
+		}
+
+		public void DeserializeData(string data)
+		{
+			count = 0;
+			int.TryParse(data, out count);
+		}
+	}
+}
