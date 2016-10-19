5c193217fb938891efa4cc70bab060e4126b0630
WolfgangKurz <wolfgangkurzdev@gmail.com>
2016-10-19 23:06:03 +0900

    전체 함재기 수 표시 수정 (슬롯이 아니라 함재기가 장착된 경우에만 표시)

--- source/Grabacr07.KanColleWrapper/Models/ShipSlot.cs
+++ source/Grabacr07.KanColleWrapper/Models/ShipSlot.cs
@@ -10,7 +10,8 @@ namespace Grabacr07.KanColleWrapper.Models
 		public SlotItem Item { get; }
 
 		public int Maximum { get; private set; }
-        public int Lost { get; private set; }
+		public int Lost { get; private set; }
+		public bool IsAirplane { get; private set; }
 
 		public bool Equipped => this.Item != null && this.Item != SlotItem.Dummy;
 
@@ -38,7 +39,22 @@ namespace Grabacr07.KanColleWrapper.Models
 			this.Item = item ?? SlotItem.Dummy;
 			this.Maximum = maximum;
 			this.Current = current;
-            this.Lost = Maximum - Current;
-        }
+			this.Lost = Maximum - Current;
+
+			var x = item.Info.Type;
+			this.IsAirplane = x == SlotItemType.艦上戦闘機
+				|| x == SlotItemType.艦上爆撃機
+				|| x == SlotItemType.艦上攻撃機
+				|| x == SlotItemType.艦上偵察機
+				|| x == SlotItemType.水上偵察機
+				|| x == SlotItemType.水上爆撃機
+				|| x == SlotItemType.オートジャイロ
+				|| x == SlotItemType.対潜哨戒機
+				|| x == SlotItemType.大型飛行艇
+				|| x == SlotItemType.水上戦闘機
+				|| x == SlotItemType.陸上攻撃機
+				|| x == SlotItemType.局地戦闘機
+				|| x == SlotItemType.艦上偵察機_II;
+		}
 	}
 }
