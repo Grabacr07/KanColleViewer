f92de8423c427f85fe0c3b0a70b29a2726015a0f
WolfgangKurz <wolfgangkurzdev@gmail.com>
2016-10-24 14:26:31 +0900

    get property 및 공백 수정

--- source/Grabacr07.KanColleWrapper/Models/Quest.cs
+++ source/Grabacr07.KanColleWrapper/Models/Quest.cs
@@ -18,13 +18,7 @@ namespace Grabacr07.KanColleWrapper.Models
 		/// <summary>
 		/// 任務の種類 (1 回のみ、デイリー、ウィークリー) を取得します。5 is daily. I don't know why
 		/// </summary>
-		public QuestType Type
-		{
-			get
-			{
-				return (QuestType)this.RawData.api_type;
-			}
-		}
+		public QuestType Type => (QuestType)this.RawData.api_type;
 
 		/// <summary>
 		/// 任務の状態を取得します。
@@ -45,12 +39,12 @@ namespace Grabacr07.KanColleWrapper.Models
 		/// 任務の詳細を取得します。
 		/// </summary>
 		public string Detail => KanColleClient.Current.Translations.GetTranslation(RawData.api_detail, TranslationType.QuestDetail, false, this.RawData, RawData.api_no)
-                                    .Replace("<br>", Environment.NewLine);
+									.Replace("<br>", Environment.NewLine);
 
-        /// <summary>
-        /// 임무 화면에서의 페이지 번호
-        /// </summary>
-        public int Page { get; set; }
+		/// <summary>
+		/// 임무 화면에서의 페이지 번호
+		/// </summary>
+		public int Page { get; set; }
 
 		public Quest(kcsapi_quest rawData) : base(rawData) { }
 
