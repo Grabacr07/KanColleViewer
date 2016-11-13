34a9c036e3dc35227c4f04ec9e5d13e881815ef8
WolfgangKurz <wolfgangkurzdev@gmail.com>
2016-11-13 22:07:27 +0900

    보급 필요량 결혼 후 15% 절약 추가

--- source/Grabacr07.KanColleWrapper/Models/Ship.cs
+++ source/Grabacr07.KanColleWrapper/Models/Ship.cs
@@ -153,7 +153,7 @@ namespace Grabacr07.KanColleWrapper.Models
 			{
 				this._Fuel = value;
 				this.RaisePropertyChanged();
-                this.RaisePropertyChanged("UsedFuel");
+				this.RaisePropertyChanged("UsedFuel");
 			}
 		}
 
@@ -170,8 +170,8 @@ namespace Grabacr07.KanColleWrapper.Models
 			{
 				this._Bull = value;
 				this.RaisePropertyChanged();
-                this.RaisePropertyChanged("UsedBull");
-            }
+				this.RaisePropertyChanged("UsedBull");
+			}
 		}
 
 		#endregion
@@ -386,14 +386,14 @@ namespace Grabacr07.KanColleWrapper.Models
 			}
 		}
 
-        #endregion
+		#endregion
 
-        #region Used 의존변수
+		#region Used 의존변수
 
-        public int UsedFuel => this.Fuel.Maximum - this.Fuel.Current;
-        public int UsedBull => this.Bull.Maximum - this.Bull.Current;
+		public int UsedFuel => (int)((this.Level > 99 ? 1.0f : 0.85f) * (this.Fuel.Maximum - this.Fuel.Current));
+		public int UsedBull => (int)((this.Level > 99 ? 1.0f : 0.85f) * (this.Bull.Maximum - this.Bull.Current));
 
-        #endregion
+		#endregion
 
 		/// <summary>
 		/// 装備によるボーナスを含めた索敵ステータス値を取得します。
