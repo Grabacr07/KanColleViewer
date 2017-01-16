fe6ff22b1ab94185bf1b255cecc61f5b71701665
WolfgangKurz <wolfgangkurzdev@gmail.com>
2017-01-16 14:10:21 +0900

    TopMost for catalogs removed, Preset prepare

---
+++ source/Grabacr07.KanColleViewer/ViewModels/Catalogs/PresetViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Grabacr07.KanColleWrapper;
+using Grabacr07.KanColleWrapper.Models;
+using MetroTrilithon.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Grabacr07.KanColleViewer.ViewModels.Contents;
+
+namespace Grabacr07.KanColleViewer.ViewModels.Catalogs
+{
+	public class PresetViewModel : WindowViewModel
+	{
+	}
+}
