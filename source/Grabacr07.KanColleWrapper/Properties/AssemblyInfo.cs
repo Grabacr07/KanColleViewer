685a70da63e48e75a723c4c6c896ae687763ec31
Grabacr07 <grabacr07+github@gmail.com>
2017-02-11 03:44:06 +0900

    version 4.2.10

--- source/Grabacr07.KanColleWrapper/Properties/AssemblyInfo.cs
+++ source/Grabacr07.KanColleWrapper/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("KanColleWrapper")]
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("8A13AB28-4E9F-423D-80AD-23EEF2821959")]
 
-[assembly: AssemblyVersion("1.6.2")]
-[assembly: AssemblyInformationalVersion("1.6.2")]
+[assembly: AssemblyVersion("1.7.0")]
+[assembly: AssemblyInformationalVersion("1.7.0")]
