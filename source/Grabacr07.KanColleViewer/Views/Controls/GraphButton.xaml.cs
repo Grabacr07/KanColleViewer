d444e6e36d692995cb41853074ef86715d28ead2
WolfgangKurz <wolfgangkurzdev@gmail.com>
2017-02-28 22:05:05 +0900

    GraphButton MouseOver effect added

--- source/Grabacr07.KanColleViewer/Views/Controls/GraphButton.xaml.cs
+++ source/Grabacr07.KanColleViewer/Views/Controls/GraphButton.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Grabacr07.KanColleWrapper;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Grabacr07.KanColleViewer.Views.Controls
 {
@@ -23,6 +15,36 @@ namespace Grabacr07.KanColleViewer.Views.Controls
 		public GraphButton()
 		{
 			InitializeComponent();
+		}
+		protected override void OnMouseEnter(MouseEventArgs e)
+		{
+			base.OnMouseEnter(e);
+
+			this.ChangeBackground();
+		}
+		protected override void OnMouseLeave(MouseEventArgs e)
+		{
+			base.OnMouseLeave(e);
+
+			this.ChangeBackground();
+		}
+		private void ChangeBackground()
+		{
+			if (this.IsMouseOver)
+			{
+				try
+				{
+					this.Background = this.FindResource("ActiveBackgroundBrushKey") as Brush;
+				}
+				catch (ResourceReferenceKeyNotFoundException ex)
+				{
+					Debug.WriteLine(ex);
+				}
+			}
+			else
+			{
+				this.Background = Brushes.Transparent;
+			}
 		}
 	}
 }
