7df7735c8ab0d8f3797fef80eb1efb0c8bf28337
WolfgangKurz <wolfgangkurzdev@gmail.com>
2017-03-02 23:53:07 +0900

    Update for AirBase on Fleets Tab

---
+++ source/Grabacr07.KanColleViewer/ViewModels/Contents/Fleets/AirBaseTroopViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grabacr07.KanColleWrapper.Models.Raw;
+using Grabacr07.KanColleWrapper.Models;
+using Grabacr07.KanColleWrapper;
+using System.Reactive.Linq;
+
+namespace Grabacr07.KanColleViewer.ViewModels.Contents.Fleets
+{
+	public class AirBaseTroopViewModel : ItemViewModel
+	{
+		public string Name => "기지항공대";
+
+		private AirBase[] _AirBases;
+		public AirBase[] AirBases
+		{
+			get { return this._AirBases; }
+			set
+			{
+				this._AirBases = value;
+				this.RaisePropertyChanged();
+			}
+		}
+
+		public AirBaseTroopViewModel(KanColleProxy proxy)
+		{
+			var homeport = KanColleClient.Current.Homeport;
+
+			// 기항대 정보를 불러옴
+			proxy.ApiSessionSource.Where(x => x.Request.PathAndQuery == "/kcsapi/api_get_member/mapinfo")
+				.TryParse<kcsapi_mapinfo_airbase>()
+				.Where(x => x.IsSuccess)
+				.Subscribe(x =>
+				{
+					this.AirBases = x.Data.api_air_base
+						.Select(y => new AirBase(y, homeport))
+						.ToArray();
+				});
+
+			// 기항대 상태 변경
+			proxy.ApiSessionSource
+				.Where(x => x.Request.PathAndQuery == "/kcsapi/api_req_air_corps/set_action")
+				.TryParse< kcsapi_empty_result>()
+				.Where(x=>x.IsSuccess)
+				.Subscribe(x =>
+				{
+					int actionKind;
+					int base_id;
+
+					if (!int.TryParse(x.Request["api_base_id"], out base_id)) return;
+					base_id--;
+
+					if (!int.TryParse(x.Request["api_action_kind"], out actionKind)) return;
+
+					this.AirBases[base_id].UpdateActionKind((AirBaseAction)actionKind);
+				});
+
+			// 기항대 보급
+			proxy.ApiSessionSource.Where(x => x.Request.PathAndQuery == "/kcsapi/api_req_air_corps/supply")
+				.TryParse<kcsapi_airbase_corps_supply>()
+				.Where(x => x.IsSuccess)
+				.Subscribe(x =>
+				{
+					int base_id;
+
+					if (!int.TryParse(x.Request["api_base_id"], out base_id)) return;
+					base_id--;
+
+					this.AirBases[base_id].Update(x.Data);
+				});
+
+			// 기항대 변경
+			proxy.ApiSessionSource.Where(x => x.Request.PathAndQuery == "/kcsapi/api_req_air_corps/set_plane")
+				.TryParse<kcsapi_airbase_corps_set_plane>()
+				.Where(x => x.IsSuccess)
+				.Subscribe(x =>
+				{
+					int base_id;
+
+					if (!int.TryParse(x.Request["api_base_id"], out base_id)) return;
+					base_id--;
+
+					this.AirBases[base_id].Update(x.Data);
+				});
+		}
+	}
+}
---
+++ source/Grabacr07.KanColleWrapper/Models/AirBase.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grabacr07.KanColleWrapper.Models.Raw;
+
+namespace Grabacr07.KanColleWrapper.Models
+{
+	public class AirBase : RawDataWrapper<kcsapi_airbase_corps>, IIdentifiable
+	{
+		private Homeport homeport { get; }
+
+		public int Id => this.RawData.api_rid;
+		public string Name => this.RawData.api_name;
+		public int Distance => this.RawData.api_distance;
+		public AirBaseAction ActionKind => (AirBaseAction)this.RawData.api_action_kind;
+
+		public AirBasePlane[] Planes { get; private set; }
+		public int AirSuperiorityPotential { get; private set; }
+
+		public AirBase(kcsapi_airbase_corps rawData, Homeport homeport) : base(rawData)
+		{
+			this.homeport = homeport;
+			this.Update(RawData);
+		}
+
+		public void Update(kcsapi_airbase_corps rawData)
+		{
+			var array = new AirBasePlane[4];
+			foreach (var item in this.RawData.api_plane_info)
+				array[item.api_squadron_id - 1] = new AirBasePlane(item, homeport.Itemyard.SlotItems[item.api_slotid]);
+
+			this.Planes = array;
+		}
+		public void Update(kcsapi_airbase_corps_set_plane rawData)
+		{
+			this.RawData.api_distance = rawData.api_distance;
+
+			var array = new AirBasePlane[4];
+			foreach (var item in this.RawData.api_plane_info)
+				array[item.api_squadron_id - 1] = new AirBasePlane(item, homeport.Itemyard.SlotItems[item.api_slotid]);
+
+			this.Planes = array;
+		}
+		public void Update(kcsapi_airbase_corps_supply rawData)
+		{
+			this.RawData.api_distance = rawData.api_distance;
+
+			var array = new AirBasePlane[4];
+			foreach (var item in this.RawData.api_plane_info)
+				array[item.api_squadron_id - 1] = new AirBasePlane(item, homeport.Itemyard.SlotItems[item.api_slotid]);
+
+			this.Planes = array;
+		}
+		public void UpdateActionKind(AirBaseAction action)
+		{
+			this.RawData.api_action_kind = (int)action;
+		}
+	}
+
+	public class AirBasePlane : RawDataWrapper<kcsapi_plane_info>, IIdentifiable
+	{
+		public SlotItem Source { get; }
+
+		public int Id => this.RawData.api_squadron_id;
+		public string Name => this.Source?.NameWithLevel ?? "";
+		public int State => this.RawData.api_state;
+
+		public int MaxCount => this.RawData.api_max_count;
+		public int CurrentCount => this.RawData.api_count;
+		public int LostCount => this.MaxCount - this.CurrentCount;
+
+		public ConditionType Condition => ConditionTypeHelper.ToConditionType(this.RawData.api_cond);
+
+		public AirBasePlane(kcsapi_plane_info rawData, SlotItem slotitem) : base(rawData)
+		{
+			this.Source = slotitem;
+		}
+	}
+
+	public enum AirBaseAction
+	{
+		대기 = 0,
+		출격 = 1,
+		방공 = 2,
+		퇴피 = 3,
+		휴식 = 4
+	}
+}
