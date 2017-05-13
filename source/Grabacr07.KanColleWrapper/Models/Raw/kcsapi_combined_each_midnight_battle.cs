cc463427f0733287c26ddbc974eec998456fea6a
WolfgangKurz <wolfgangkurzdev@gmail.com>
2017-05-14 04:28:24 +0900

    Raw models, ResourceLogWindow resources value fix

---
+++ source/Grabacr07.KanColleWrapper/Models/Raw/kcsapi_combined_each_midnight_battle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grabacr07.KanColleWrapper.Models.Raw
+{
+	public class kcsapi_combined_each_midnight_battle
+	{
+		public int[] api_active_deck { get; set; }
+		public int api_deck_id { get; set; }
+		public int[] api_ship_ke { get; set; }
+		public int[] api_ship_ke_combined { get; set; }
+		public int[] api_ship_lv { get; set; }
+		public int[] api_ship_lv_combined { get; set; }
+		public int[] api_nowhps { get; set; }
+		public int[] api_maxhps { get; set; }
+		public int[] api_nowhps_combined { get; set; }
+		public int[] api_maxhps_combined { get; set; }
+		public int[][] api_eSlot { get; set; }
+		public int[][] api_eSlot_combined { get; set; }
+		public int[][] api_eKyouka { get; set; }
+		public int[][] api_fParam { get; set; }
+		public int[][] api_eParam { get; set; }
+		public int[][] api_fParam_combined { get; set; }
+		public int[][] api_eParam_combined { get; set; }
+		public int[] api_touch_plane { get; set; }
+		public int[] api_flare_pos { get; set; }
+		public kcsapi_data_midnight_hougeki api_hougeki { get; set; }
+	}
+}
