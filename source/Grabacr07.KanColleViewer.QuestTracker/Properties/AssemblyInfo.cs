d258b3ee680b34eaffd33dc3aeed89a7c3ae0fb1
WolfgangKurz <wolfgangkurzdev@gmail.com>
2017-06-06 19:00:07 +0900

    Update versions, Update quest-tracker

--- source/Grabacr07.KanColleViewer.QuestTracker/Properties/AssemblyInfo.cs
+++ source/Grabacr07.KanColleViewer.QuestTracker/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("039E4D88-DECD-494D-92D2-407C6403A010")]
 
-[assembly: AssemblyVersion("1.0.14")]
-[assembly: AssemblyInformationalVersion("1.0.14")]
+[assembly: AssemblyVersion("1.0.15")]
+[assembly: AssemblyInformationalVersion("1.0.15")]
