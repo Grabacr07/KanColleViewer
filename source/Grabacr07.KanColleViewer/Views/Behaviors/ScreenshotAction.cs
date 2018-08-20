5b74f70af4b0b418548a120d4c28b2e0d93ec8c2
Grabacr07 <grabacr07+github@gmail.com>
2018-08-20 22:53:16 +0900

    お掃除

--- source/Grabacr07.KanColleViewer/Views/Behaviors/ScreenshotAction.cs
+++ source/Grabacr07.KanColleViewer/Views/Behaviors/ScreenshotAction.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reactive;
 using System.Threading.Tasks;
-using System.Windows.Media.Imaging;
 using CefSharp;
 using CefSharp.Wpf;
 using Grabacr07.KanColleViewer.Models;
@@ -14,62 +12,33 @@ using Grabacr07.KanColleViewer.Properties;
 using Grabacr07.KanColleViewer.ViewModels.Messages;
 using Livet.Behaviors.Messaging;
 using Livet.Messaging;
-using Size = System.Windows.Size;
 
 namespace Grabacr07.KanColleViewer.Views.Behaviors
 {
 	/// <summary>
 	/// 艦これのゲーム部分を画像として保存する機能を提供します。
 	/// </summary>
-	internal class ScreenshotAction : InteractionMessageAction<ChromiumWebBrowserWithScreenshotSupport>
+	internal class ScreenshotAction : InteractionMessageAction<ChromiumWebBrowser>
 	{
 		private IFrame targetCanvas;
 
 		protected override async void InvokeAction(InteractionMessage message)
 		{
-			var screenshotMessage = message as ScreenshotMessage;
-			if (screenshotMessage == null)
+			if (message is ScreenshotMessage screenshotMessage)
 			{
-				return;
-			}
-			
-			try
-			{
-				await this.TakeScreenshotFromCanvas(screenshotMessage.Path, screenshotMessage.Format);
-				StatusService.Current.Notify(Resources.Screenshot_Saved + Path.GetFileName(screenshotMessage.Path));
-			}
-			catch (Exception ex)
-			{
-				StatusService.Current.Notify(Resources.Screenshot_Failed + ex.Message);
+				try
+				{
+					await this.TakeScreenshot(screenshotMessage.Path, screenshotMessage.Format);
+					StatusService.Current.Notify(Resources.Screenshot_Saved + Path.GetFileName(screenshotMessage.Path));
+				}
+				catch (Exception ex)
+				{
+					StatusService.Current.Notify(Resources.Screenshot_Failed + ex.Message);
+				}
 			}
 		}
 
-		private async Task TakeScreenshotFromCef(string path, SupportedImageFormat format)
-		{
-			const string notFoundMessage = "艦これの HTML 要素が見つかりません。";
-			const string script = "(function() { var element = document.getElementById('game_frame'); return [element.clientWidth, element.clientHeight]; })();";
-
-			var frame = this.AssociatedObject.GetBrowser()?.MainFrame;
-			if (frame == null) throw new Exception(notFoundMessage);
-
-			var response = await frame.EvaluateScriptAsync(script);
-			if (!response.Success) throw new Exception(notFoundMessage);
-
-			var list = (List<object>)response.Result;
-			var size = new Size((int)list[0], (int)list[1]);
-
-			var bitmap = await this.AssociatedObject.TakeScreenshot(size, timeout: TimeSpan.FromSeconds(5));
-			using (var stream = new FileStream(path, FileMode.Create))
-			{
-				var encoder = format.ToBitmapEncoder();
-				if (encoder == null) throw new ArgumentException($"{format} 形式はサポートされていません。");
-
-				encoder.Frames.Add(BitmapFrame.Create(bitmap));
-				encoder.Save(stream);
-			}
-		}
-
-		private Task TakeScreenshotFromCanvas(string path, SupportedImageFormat format)
+		private Task TakeScreenshot(string path, SupportedImageFormat format)
 		{
 			var source = new TaskCompletionSource<Unit>();
 
@@ -99,5 +68,4 @@ namespace Grabacr07.KanColleViewer.Views.Behaviors
 			return source.Task;
 		}
 	}
-
 }
