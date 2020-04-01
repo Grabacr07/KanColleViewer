028a5d5f04e963d623e7b3fec9312774fa9041c4
lasedark <laserdark@outlook.com>
2020-04-01 15:43:32 +0800

    update version

--- source/Grabacr07.KanColleViewer/Properties/AssemblyInfo.cs
+++ source/Grabacr07.KanColleViewer/Properties/AssemblyInfo.cs
@@ -15,4 +15,4 @@ using System.Windows;
 	ResourceDictionaryLocation.None,
 	ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("4.5.2.110")]
+[assembly: AssemblyVersion("4.5.2.111")]
